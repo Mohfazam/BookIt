02cee05d377b1b97d130dbeb7e8c7d18126f16ed
Mohfazam <mohd.sarwar.code@gmail.com>
2025-10-31 04:29:00 +0530

    Image and checkout card

--- frontend/src/app/experience/[id]/page.tsx
+++ frontend/src/app/experience/[id]/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from 'react';
 import { useParams } from 'next/navigation';
-import { ArrowLeft } from "lucide-react";
+import { ArrowLeft, Minus, Plus } from "lucide-react";
 import Link from 'next/link';
 
 interface Slot {
@@ -30,6 +30,7 @@ export default function ExperienceDetailPage() {
     const [experience, setExperience] = useState<ExperienceDetail | null>(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<string | null>(null);
+    const [Quantity, setQuantity] = useState(1);
 
     useEffect(() => {
         if (id) {
@@ -82,12 +83,59 @@ export default function ExperienceDetailPage() {
     return (
         <div className="max-w-7xl mx-auto px-4 py-8 sm:px-6 lg:px-8">
             <div className="space-y-4">
-                <Link href={"/"}>
-                <div className='flex w-[74px] h-[20px] text-[#000000] justify-between items-center hover:cursor-pointer'>
-                    <ArrowLeft size={20} />
-                    <span className='block text-[14px] leading-[18px]'>Details</span>
+                <div className='flex w-[74px] h-[20px] text-[#000000] justify-between items-center hover:cursor-pointer py-6'>
+                    <Link href={"/"} className='flex justify-between items-center w-[74px] h-[20px]'>
+                        <ArrowLeft size={20} />
+                        <span className='block text-[14px] leading-[18px]'>Details</span>
+                    </Link>
                 </div>
-                </Link>
+
+                <div className='flex gap-8'>
+
+
+                    <div className='w-[765px] h-[381px] rounded-[12px] overflow-hidden'>
+                        <img src={experience.image} alt={experience.title} className='w-full h-full object-cover' draggable={false}/>
+                    </div>
+
+
+                    <div className='w-[387px] h-[303px] px-[24px] py-[24px] rounded-[12px] bg-[#EFEFEF] flex flex-col gap-4'>
+                        <div className='w-[339px] h-[22px] flex justify-between items-center'>
+                            <span className='block w-[65px] h-[20px] text-[16px] leading-[20px] text-[#656565]'>Starts at</span>
+                            <span className='w-[44px] h-[22px] text-[18px] leading-[22px] text-[#161616] text-right'>₹{experience.price}</span>
+                        </div>
+
+                        <div className='w-[339px] h-[22px] flex justify-between items-center'>
+                            <span className='block w-[65px] h-[20px] text-[16px] leading-[20px] text-[#656565]'>Quantity</span>
+                            <div className='w-[44px] h-[22px] text-[18px] leading-[22px] text-[#161616] flex items-center gap-2'>
+                                <Minus size={16} className='hover:cursor-pointer' onClick={() => setQuantity(Quantity-1)}/>
+                                    <span className='block w-[6px] h-[14px] text-[12px] leading-[14px]'>{Quantity}</span>
+                                <Plus size={16} className='hover:cursor-pointer' onClick={() => setQuantity(Quantity+1)}/>
+                            </div>
+                        </div>
+
+                        <div className='w-[339px] h-[22px] flex justify-between items-center'>
+                            <span className='block w-[65px] h-[20px] text-[16px] leading-[20px] text-[#656565]'>Subtotal</span>
+                            <span className='w-[44px] h-[22px] text-[18px] leading-[22px] text-[#161616] text-right'>₹{experience.price * Quantity}</span>
+                        </div>
+
+                        <div className='w-[339px] h-[22px] flex justify-between items-center'>
+                            <span className='block w-[65px] h-[20px] text-[16px] leading-[20px] text-[#656565]'>Taxes</span>
+                            <span className='w-[44px] h-[22px] text-[18px] leading-[22px] text-[#161616] text-right'>₹59</span>
+                        </div>
+
+                        <div className='w-[339px] h-[1px] bg-[#D9D9D9] '/>
+
+                        <div className='w-[339px] h-[24px] flex justify-between items-center'>
+                            <span className='block w-[48px] h-[24px] text-[20px] leading-[24px] font-bold text-[#161616]'>Total</span>
+                            <span className='w-[44px] h-[22px] text-[20px] leading-[24px] text-[#161616] font-bold text-right'>₹{ (experience.price * Quantity ) + 59}</span>
+                        </div>
+
+                        <div className='w-[339px] h-[44px] px-[20px] py-[12px]  rounded-[8px] bg-[#FFD643]'>
+                            <span className='block font-normal w-[62px] h-[20px] leading-[20px] text-[#161616] text-center'>Confirm</span>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </div>
     );
