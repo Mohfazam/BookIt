3bb24b5e0feae3b952a570dd4b17fb4760b99909
Mohfazam <mohd.sarwar.code@gmail.com>
2025-10-30 20:35:53 +0530

    Logo

---
+++ frontend/src/Components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-[#f9f9f9] max-w-6xl mx-auto px-4 sm:px-6 lg:px-8'>
+      <div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
